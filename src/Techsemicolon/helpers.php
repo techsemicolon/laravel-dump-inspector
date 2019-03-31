9113986082cb27ce627e57464661beb7706a82e0
techsemicolon <tech.semicolon@gmail.com>
2019-04-01 03:11:42 +0530

    Updating helper function

--- src/Techsemicolon/helpers.php
+++ src/Techsemicolon/helpers.php
@@ -3,12 +3,32 @@
 use Techsemicolon\InspectorDumper;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 
-if(!function_exists('idd')){
+if(!function_exists('idump')){
 
-    function idd()
+    function idump()
     {
+        $arguments = func_get_args();
+
+        if(empty($arguments)){
+            
+            array_map(function ($x) {
+                (new InspectorDumper)->dump((new VarCloner)->cloneVar($x));
+            }, $arguments);
+            
+            return;
+        }
+
+        if(count($arguments) == 1)
+        {
+            array_map(function ($x) {
+                (new InspectorDumper)->dump((new VarCloner)->cloneVar($x));
+            }, func_get_args());
+
+            return $arguments[0];
+        }
+
         array_map(function ($x) {
             (new InspectorDumper)->dump((new VarCloner)->cloneVar($x));
-        }, func_get_args());
+        }, $arguments);
     }
 }
